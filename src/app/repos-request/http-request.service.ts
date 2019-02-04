e56ecfe241131f0d8bcba6b810451d1bff23adbe
Nicole Hategekimana <nicoleha127@gmail.com>
2019-02-04 17:15:18 +0200

    user form created

---
+++ src/app/repos-request/http-request.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {Repository} from '../repository';
+import {HttpClient} from '@angular/common/http'
+import {environment} from '../../environments/environment'
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpRequestService {
+
+  repos:Repository;
+
+constructor(private http:HttpClient) { 
+  this.repos=new Repository(0,0,0,"");
+}
+
+userRequest(){
+
+  interface ApiResponse{
+      repos:number;
+      following:number;
+      follower:number;
+      url:string;
+
+  }
+  let promise =new Promise((resolve,reject)=>{
+      this.http.get<ApiResponse>(environment.api_key).toPromise().then(response=>{
+          
+          this.repos.repos=response.repos
+          this.repos.following=response.following
+          this.repos.follower=response.follower
+          this.repos.url=response.url
+
+          resolve()
+      },
+      error=>{
+              // this.quote.quote="Never, never, never give up."
+              // this.quote.author="winston churchill"
+              reject(error)
+          }
+      )
+  })
+
+  return promise
+}
+}
